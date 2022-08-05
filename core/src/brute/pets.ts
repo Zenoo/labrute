d7092fe57b2c7dc50bd8fae1df93379daf5b3e00
Zenoo <perso@franckdemoute.fr>
2022-08-05 19:58:01 +0200

    fix: Added initiative to dog. #24

--- core/src/brute/pets.ts
+++ core/src/brute/pets.ts
@@ -4,12 +4,28 @@ export interface Pet {
   name: PetName;
   odds: number;
   enduranceMalus: number;
+  initiative: number;
 }
 
 const pets: Pet[] = [
-  { name: 'bear', odds: 1124 / 35792, enduranceMalus: 2 },
-  { name: 'panther', odds: 1118 / 35792, enduranceMalus: 6 },
-  { name: 'dog', odds: 33550 / 35792, enduranceMalus: 8 },
+  {
+    name: 'bear',
+    odds: 1124 / 35792,
+    enduranceMalus: 2,
+    initiative: -3.6,
+  },
+  {
+    name: 'panther',
+    odds: 1118 / 35792,
+    enduranceMalus: 6,
+    initiative: -0.6,
+  },
+  {
+    name: 'dog',
+    odds: 33550 / 35792,
+    enduranceMalus: 8,
+    initiative: -0.1,
+  },
 ];
 
 export default pets;
