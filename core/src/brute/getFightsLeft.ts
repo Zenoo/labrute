be53f61b16dc8573437138d81106c1a33b2b68dd
Zenoo <perso@franckdemoute.fr>
2023-01-30 12:27:45 +0100

    fix: Fights left counter. Fixes #38

--- core/src/brute/getFightsLeft.ts
+++ core/src/brute/getFightsLeft.ts
@@ -2,7 +2,7 @@ import { Brute } from '@labrute/prisma';
 import moment from 'moment';
 import { FIGHTS_PER_DAY } from '../constants';
 
-const getFightsLeft = (brute: Brute) => (moment(brute.lastFight, 'DD/MM/YYYY').isSame(moment(), 'day')
+const getFightsLeft = (brute: Brute) => (moment(brute.lastFight).isSame(moment(), 'day')
   ? brute.fightsLeft
   : FIGHTS_PER_DAY);
 
