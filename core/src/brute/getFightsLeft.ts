68d21171a81630ee793eb45296483a4b9ebe0f3f
Zenoo <perso@franckdemoute.fr>
2023-02-08 14:03:20 +0100

    fix: Fixed the regeneration skill to add 2 more fights each day

--- core/src/brute/getFightsLeft.ts
+++ core/src/brute/getFightsLeft.ts
@@ -2,8 +2,10 @@ import { Brute } from '@labrute/prisma';
 import moment from 'moment';
 import { FIGHTS_PER_DAY } from '../constants';
 
+const getMaxFights = (brute: Brute) => (brute.skills.includes('regeneration') ? FIGHTS_PER_DAY + 2 : FIGHTS_PER_DAY);
+
 const getFightsLeft = (brute: Brute) => (moment.utc(brute.lastFight).isSame(moment.utc(), 'day')
   ? brute.fightsLeft
-  : FIGHTS_PER_DAY);
+  : getMaxFights(brute));
 
 export default getFightsLeft;
