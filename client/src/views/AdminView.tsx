4b8d2eaefe19d20e91e6f4648f73ffc1adbe58ec
Franck <perso@franckdemoute.fr>
2023-11-10 22:28:45 +0100

    fix: Include user ID in admin panel when searching for a brute

--- client/src/views/AdminView.tsx
+++ client/src/views/AdminView.tsx
@@ -156,7 +156,7 @@ const AdminView = () => {
             />
             {brute && brute.body && brute.colors && (
               <>
-                <Text h2 smallCaps>{brute.name}</Text>
+                <Text h2 smallCaps>{brute.name} ({brute.userId})</Text>
                 <BruteComponent brute={brute} sx={{ width: 100 }} />
                 <Grid container spacing={1}>
                   <Grid item xs={6} sm={3}>
