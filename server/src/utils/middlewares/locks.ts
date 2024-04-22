4673b6209318d3ed49de727d4ce5da793eb7d2a0
Yohan Gastoud <yohan.g99@gmail.com>
2024-04-22 23:59:37 +0200

    fix: prevent race condition (#623)
    * fix: prevent race condition on routes
    
    * fix & refact: apply recommendations
    
    * typo: apply recommendation

---
+++ server/src/utils/middlewares/locks.ts
@@ -0,0 +1,37 @@
+import { ExpectedError } from '@labrute/core';
+import { Request, Response, NextFunction } from 'express';
+import sendError from '../sendError.js';
+
+interface Locks {
+  [key: string]: boolean | undefined
+}
+
+const locks: Locks = {};
+
+export default function lockMiddleware(req: Request, res: Response, next: NextFunction) {
+  const { method, path } = req;
+
+  const { headers: { authorization } } = req;
+
+  if (authorization) {
+    const [id] = Buffer.from(authorization.split(' ')[1], 'base64').toString().split(':');
+
+    if (!id || id === 'null') {
+      return sendError(res, new ExpectedError('Invalid authorization header content'));
+    }
+
+    const key = `${method}:${path}:${id}`;
+
+    if (locks[key]) {
+      return sendError(res, new ExpectedError('Too many requests'));
+    }
+
+    locks[key] = true;
+
+    res.on('finish', () => {
+      delete locks[key];
+    });
+  }
+
+  return next();
+}
