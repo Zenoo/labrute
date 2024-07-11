275248ae0fe28c98c56afcc97e742e1127494782
Franck <perso@franckdemoute.fr>
2024-07-11 17:28:35 +0200

    fix: Unify colors to their stats

--- client/src/utils/StatColor.ts
+++ client/src/utils/StatColor.ts
@@ -1,21 +1,24 @@
 import { FightStat } from '@labrute/core';
 
+const agilityColor = '#8686ff';
+const speedColor = '#eb9800';
+
 const StatColor: Record<FightStat, string> = {
   [FightStat.REVERSAL]: '#8fbc8f',
   [FightStat.COUNTER]: '#daa520',
   [FightStat.EVASION]: '#17a717',
-  [FightStat.SWIFTNESS]: '#ffb6c1',
-  [FightStat.BLOCK]: '#8686ff',
-  [FightStat.ACCURACY]: '#808080',
+  [FightStat.SWIFTNESS]: speedColor,
+  [FightStat.BLOCK]: '#808080',
+  [FightStat.ACCURACY]: agilityColor,
   [FightStat.DISARM]: '#ff70ff',
-  [FightStat.COMBO]: 'orange',
+  [FightStat.COMBO]: agilityColor,
   [FightStat.DEFLECT]: '#ff4500',
   [FightStat.ARMOR]: '#008080',
   [FightStat.HIT_SPEED]: '#800080',
   [FightStat.INITIATIVE]: '#f08080',
   [FightStat.STRENGTH]: 'red',
-  [FightStat.AGILITY]: '#8686ff',
-  [FightStat.SPEED]: '#eb9800',
+  [FightStat.AGILITY]: agilityColor,
+  [FightStat.SPEED]: speedColor,
   [FightStat.ENDURANCE]: '#17a717',
   [FightStat.DAMAGE]: 'red',
 } as const;
