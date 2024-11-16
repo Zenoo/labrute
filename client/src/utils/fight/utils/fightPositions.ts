88da3276ddfb4463d856ecd06fceb52bc44a1902
Franck <perso@franckdemoute.fr>
2024-11-16 16:30:07 +0100

    fix: Additional fight positions. Fixes #1039

--- client/src/utils/fight/utils/fightPositions.ts
+++ client/src/utils/fight/utils/fightPositions.ts
@@ -11,6 +11,16 @@ const leftPositions = [
   { x: 60, y: 272 },
 ];
 
+const extraLeftPositions = [
+  { x: 100, y: 200 },
+  { x: 80, y: 212 },
+  { x: 100, y: 224 },
+  { x: 80, y: 236 },
+  { x: 100, y: 248 },
+  { x: 80, y: 260 },
+  { x: 100, y: 272 },
+];
+
 const rightPositions = [
   { x: 440, y: 200 },
   { x: 460, y: 212 },
@@ -21,12 +31,31 @@ const rightPositions = [
   { x: 440, y: 272 },
 ];
 
+const extraRightPositions = [
+  { x: 400, y: 200 },
+  { x: 420, y: 212 },
+  { x: 400, y: 224 },
+  { x: 420, y: 236 },
+  { x: 400, y: 248 },
+  { x: 420, y: 260 },
+  { x: 400, y: 272 },
+];
+
 const getAvailablePositions = (fighters: AnimationFighter[], team: 'L' | 'R') => {
   const teamFighters = fighters.filter((fighter) => fighter.team === team);
-
-  return (team === 'L' ? leftPositions : rightPositions)
+  const positions = (team === 'L' ? leftPositions : rightPositions)
     .filter((p) => !teamFighters.find((f) => f.animation.container.x === p.x
     && f.animation.container.y === p.y));
+
+  if (positions.length > 0) {
+    return positions;
+  }
+
+  const extraPositions = (team === 'L' ? extraLeftPositions : extraRightPositions)
+    .filter((p) => !teamFighters.find((f) => f.animation.container.x === p.x
+    && f.animation.container.y === p.y));
+
+  return extraPositions;
 };
 
 const getRandomPosition = (fighters: AnimationFighter[], team: 'L' | 'R') => {
