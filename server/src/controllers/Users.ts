e28a9771d7826ed6702644223ed25a68d0318ba1
Franck <perso@franckdemoute.fr>
2026-04-12 16:35:56 +0200

    fix: Improve error logging during authentication process

--- server/src/controllers/Users.ts
+++ server/src/controllers/Users.ts
@@ -123,7 +123,8 @@ export const Users = {
           authResult.fingerprints.push(fingerprint);
         }
       } catch (_error) {
-        DISCORD().sendError(_error as Error, res);
+        DISCORD().logObject(req.body, _error instanceof Error ? _error.message : 'unknown error during authentication')
+          .catch(() => { /* ignore */ });
         res.send({
           modifiers: await ServerState.getModifiers(prisma),
           currentEvent: await ServerState.getCurrentEvent(prisma),
