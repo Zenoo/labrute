fae7d840a0caca73146a8c5dd07fcc2d4192d631
a.masson <adrien.masson@hotmail.fr>
2026-04-21 21:26:14 +0200

    [FIX] it & ko translate forget

--- client/src/i18n/locales/it/index.ts
+++ client/src/i18n/locales/it/index.ts
@@ -1,7 +1,11 @@
 import common from './common.json';
+import global from './global.json';
+import home from './home.json';
 
 const it = {
   common,
+  global,
+  home,
 } as const;
 
 export default it;
--- client/src/i18n/locales/ko/index.ts
+++ client/src/i18n/locales/ko/index.ts
@@ -1,7 +1,11 @@
 import common from './common.json';
+import global from './global.json';
+import home from './home.json';
 
 const ko = {
   common,
+  global,
+  home,
 } as const;
 
 export default ko;
