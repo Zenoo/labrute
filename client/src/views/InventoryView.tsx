680eb49304afc9733e5ac483e40a6c5ccaa3698a
Franck <perso@franckdemoute.fr>
2026-05-25 08:57:00 +0200

    fix: Missing boss icon

--- client/src/views/InventoryView.tsx
+++ client/src/views/InventoryView.tsx
@@ -21,7 +21,7 @@ import { catchError } from '../utils/catchError';
 
 const itemImage: Record<InventoryItemType, string> = {
   [InventoryItemType.visualReset]: '/images/inventory/color-reset.svg',
-  [InventoryItemType.bossTicket]: '/images/inventory/bossTicket.png',
+  [InventoryItemType.bossTicket]: '/images/inventory/bossTicket.webp',
   [InventoryItemType.nameChange]: '/images/inventory/nameChange.png',
   [InventoryItemType.favoriteFight]: '/images/inventory/favoriteFight.webp',
   [InventoryItemType.customizationToken]: '/images/inventory/customizationToken.png',
