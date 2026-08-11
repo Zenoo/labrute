317057eefef29bd375d949f672d5f342836b6ea2
Franck <perso@franckdemoute.fr>
2026-08-11 13:37:59 +0200

    fix: Update dogs in small pet list

--- client/src/components/Brute/BruteSmallPetList.tsx
+++ client/src/components/Brute/BruteSmallPetList.tsx
@@ -19,7 +19,7 @@ export const BruteSmallPetList = ({
   <Box sx={sx} {...rest}>
     {entries(brutePets).map(([pet, tier]) => (
       <PetTooltip brute={brute} pet={pets[pet]} tier={tier} key={pet}>
-        <Box component="img" src={`/images/pets/${pet.replace(/\d/g, '')}.svg`} sx={{ width: 16, m: 0.25, mb: 0, filter: tier > 1 ? `drop-shadow(1px 1px 1px black) drop-shadow(0 -0.1px 0 ${TieredPerkColor[tier]}) drop-shadow(0.1px 0 0 ${TieredPerkColor[tier]}) drop-shadow(0 0.1px 0 ${TieredPerkColor[tier]}) drop-shadow(-0.1px 0 0 ${TieredPerkColor[tier]})` : 'drop-shadow(1px 1px 1px black)' }} />
+        <Box component="img" src={`/images/pets/${pet}.svg`} sx={{ width: 16, m: 0.25, mb: 0, filter: tier > 1 ? `drop-shadow(1px 1px 1px black) drop-shadow(0 -0.1px 0 ${TieredPerkColor[tier]}) drop-shadow(0.1px 0 0 ${TieredPerkColor[tier]}) drop-shadow(0 0.1px 0 ${TieredPerkColor[tier]}) drop-shadow(-0.1px 0 0 ${TieredPerkColor[tier]})` : 'drop-shadow(1px 1px 1px black)' }} />
       </PetTooltip>
     ))}
   </Box>
